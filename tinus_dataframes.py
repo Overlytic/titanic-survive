613b7301fdac76e0c0f43a3704e544153ecc1ff5
Ser-JNB-Moses-Worker <Ser-JNB-MosesWorker@eu.munichre.com>
2016-10-19 18:18:52 +0200

    added to the dataframe file

--- tinus_dataframes.py
+++ tinus_dataframes.py
@@ -69,3 +69,25 @@ print '------------------'
 df['Age'].dropna().hist(bins=16, range=(0,80), alpha = .5)
 pyplot.show()
 
+# The Data cleaning Starts....
+
+#create new Gender column... hard to work with string values male and female
+print '------------------'
+print "Create gender column with value 4 "
+print '------------------'
+df['Gender'] = 4
+print df.Gender.describe()
+
+#lambda... from lambda calculus... create a anonymous function at runtime. Here takes the first char
+print '------------------'
+print "Set gender column = to first char of Sex column (uppercase)"
+print '------------------'
+df['Gender'] = df['Sex'].map(lambda x: x[0].upper())
+print df.head(3)
+
+print '------------------'
+print "Gender set to 0->female, 1-> male"
+print '------------------'
+df['Gender'] = df['Sex'].map({'female': 0, 'male': 1}).astype(int)
+print df.Gender.describe()
+
